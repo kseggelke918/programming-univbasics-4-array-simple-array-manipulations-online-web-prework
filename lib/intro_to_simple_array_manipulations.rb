ee88765e0bf09f878be9767a42cfab862d717881
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-27 17:08:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -43,4 +43,14 @@ def using_delete(instructors, instructor_to_remove)
   instructors.delete(instructor_to_remove)
 end 
 
+def using_delete_at(famous_robots, 2)
+  
+end
 
+describe "using_delete_at" do 
+  it "takes in two arguments, an array and an integer and deletes the element at the index of the array that is equal to that integer" do 
+    famous_robots = ["Johnny 5", "R2D2", "Robocop"]
+    deleted_robot = using_delete_at(famous_robots, 2)
+    expect(deleted_robot).to eq("Robocop")
+  end
+end
