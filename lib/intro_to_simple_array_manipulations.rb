5e725994b4273ab5c0adec9fb175357e3ac315dd
Katie Seggelke <katie.seggelke@gmail.com>
2019-06-27 16:26:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,7 +2,7 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)  
 end
 
-def using_unshift
-
+def using_unshift(bouroughs_in_nyc, @new_neighborhood)
+  
 end
     
